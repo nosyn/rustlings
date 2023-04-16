5296571ed04b95aae5899d0881b2e7a1b09ad324
Son Yen Nguyen <nosyn@Sons-MBP.attlocal.net>
2023-04-16 16:21:45 -0500

    hashmaps

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -57,6 +55,7 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         team_2.goals_scored += team_2_score;
         team_2.goals_conceded += team_1_score;
     }
+
     scores
 }
 
